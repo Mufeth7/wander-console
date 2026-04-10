7fc3b6c4eaec0f584c666ebdd956b93a85e3dd52
Syed Mufeth <smufeth7@gmail.com>
2026-04-10 13:48:05 +0530

    added new console

--- wander.js
+++ wander.js
@@ -1,7 +1,8 @@
 window.wander = {
   // Other Wander consoles that visitors can reach from my console.
   consoles: [
-    '',
+    'https://mufeth7.github.io/wander-console/',
+    'https://karthikeyan2k3.github.io/wander-console/'
   ],
   // Websites and pages I recommend to other wanderers.
   pages: [
