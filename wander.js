ee5494bb9fdcda28a283e3e9eb61c02c9107bf65
Syed Mufeth <smufeth7@gmail.com>
2026-04-10 14:01:26 +0530

    added susam wander file

--- wander.js
+++ wander.js
@@ -2,7 +2,8 @@ window.wander = {
   // Other Wander consoles that visitors can reach from my console.
   consoles: [
     'https://mufeth7.github.io/wander-console/',
-    'https://karthikeyan2k3.github.io/wander-console/'
+    'https://karthikeyan2k3.github.io/wander-console/',
+    'https://susam.net/wander/wander.js'
   ],
   // Websites and pages I recommend to other wanderers.
   pages: [
