9127dc3b271767169c15324feb8f6183715f4ce7
Syed Mufeth <smufeth7@gmail.com>
2026-04-10 09:56:32 +0530

    add pages

---
+++ wander.js
@@ -0,0 +1,12 @@
+window.wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    '',
+  ],
+  // Websites and pages I recommend to other wanderers.
+  pages: [
+    'https://mufeth.space/',
+    'https://cyrneko.eu/',
+    'https://starlightnet.work/'
+  ],
+}
